8c0b69741452ce5ebd791efacf332cec978e6356
Bryanlover1 <165657669+Bryanlover1@users.noreply.github.com>
2024-06-20 02:22:03 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,7 +7,7 @@ const { default: axios } = require('axios');
 zokou({ nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '🚀', 
+    reaction: '⚡', 
     fromMe: 'true', 
 
        
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*𝐌𝐞𝐠𝐚𝐭𝐫𝐨𝐧!!*\n ```' + 7000 + '``` *𝐌𝐬*') 
+    return repondre('*𝐋𝐔𝐅𝐅𝐘!!*\n ```' + 70.0.2 + '``` *𝐌𝐬*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
